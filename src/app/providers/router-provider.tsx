ba0b0a65adcb54080ee91619ae9eecd2e09581e9
Vladimir Bavtenko <batonich84@mail.ru>
2025-10-21 11:01:47 +0300

    fix: Move to shared router config.

--- src/app/providers/router-provider.tsx
+++ src/app/providers/router-provider.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from 'react-router-dom'
-import { appRouterConfig } from '../router/config.tsx'
+import { appRouterConfig } from '@/shared/config/router-config/routerConfig.tsx'
 
 export const RouterProviderComponent = () => {
   return <RouterProvider router={appRouterConfig} />
